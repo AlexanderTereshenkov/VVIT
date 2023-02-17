8ee8bac94c3216ffd6e2e6ccc5ba3b238d7600be
AlexanderTereshenkov <alexandertereshenkov@mail.ru>
2023-02-18 00:53:11 +0300

    new test

--- weather/main.py
+++ weather/main.py
@@ -1,1 +1,27 @@
+import requests
+
+city = "MOSCOW,RU"
+key = "2a3842a855e0a311a8c38f797c10aceb"
+resultWeek = requests.get("http://api.openweathermap.org/data/2.5/forecast", params={'q': city, 'units': 'metric',
+                                                                              'lang': 'ru', 'APPID': key})
+resultDay = requests.get("http://api.openweathermap.org/data/2.5/weather", params={'q': city, 'units': 'metric',
+                                                                             'lang': 'ru', 'APPID': key})
+
+data1 = resultWeek.json()
+data2 = resultDay.json()
+
+
+def weak_report():
+    print("Скорость ветра и видимость на несколько дней")
+    for i in data1['list']:
+        print(str(i['wind']['speed']) + "m/s " + str(i['visibility']) + "m " + str(i['dt_txt']) + "\n")
+
+
+def day_report():
+    print("Скорость ветра и видимость на текущий день")
+    print(data2['wind']['speed'])
+
+
+weak_report()
+day_report()
 
